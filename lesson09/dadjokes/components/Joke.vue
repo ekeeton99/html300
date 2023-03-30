d5ea9609ef886cd7d0c7aebb429d31d934604165
= <=>
2023-03-29 20:40:45 -0700

    Finished nuxt crash course

--- lesson09/dadjokes/components/Joke.vue
+++ lesson09/dadjokes/components/Joke.vue
@@ -1,7 +1,9 @@
 <template>
+<nuxt-link :to="'jokes/' + id">
   <div class="joke">
-    <p>Jokes</p>
+    <p>{{ joke }}</p>
   </div>
+</nuxt-link>
 </template>
 
 <script>
--- lesson09/dadjokes/pages/Jokes/index.vue
+++ lesson09/dadjokes/pages/Jokes/index.vue
@@ -1,45 +1,57 @@
 <template>
     <div>
-        <Joke />
+        <searchJokes v-on:search-text="search-text" />
+        <Joke v-for="joke in jokes"
+        :key="joke.id"
+        :id="joke.id"
+        :joke="joke.joke" />
     </div>
 </template>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
 import Joke from "../../components/Joke";
+import searchJokes from "../../components/searchJokes";
 
 export default {
     components: {
-        Joke
+        Joke,
+        searchJokes
     },
-//     data() {
-//         return {
-//             jokes: []
-//         };
-//     },
-    // mounted() {
-    //     axios
-    //         .get('https://icanhazdadjoke.com/search')
-    //         .then(response => (this.jokes = response.data.results))
-    //         .catch(error => {
-    //             console.log(error)
-    //         })
-    //         .finally(() => this.loading = false)
-    // },
-    // async created() {
-    //     const config = {
-    //         headers: {
-    //             Accept: "application/json"
-    //         }
-    //     };
+    data() {
+        return {
+            jokes: []
+        };
+    },
+    async created() {
+        const config = {
+            headers: {
+                Accept: "application/json"
+            }
+        };
 
-    //     try {
-    //         const response = await axios.get('https://icanhazdadjoke.com/search', config);
-    //         this.jokes = response.data.results;
-    //     } catch(error) {
-    //         console.log(error);
-    //     }
-    // },
+        try {
+            const res = await axios.get('https://icanhazdadjoke.com/search', config);
+            this.jokes = res.data.results;
+        } catch(error) {
+            console.log(error);
+        }
+    },
+    methods: {
+        async searchText(text) {
+            const config = {
+                headers: {
+                    Accept: "application/json"
+                }
+            };
+            try {
+                const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config);
+                this.jokes = res.data.results;
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    },
     head() {
         return {
             title: "Jokes",
