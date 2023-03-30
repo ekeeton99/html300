f32aac69048dc8fb33a2b6533a5466be4210476f
= <=>
2023-03-29 19:50:58 -0700

    Fixed git submodule

---
+++ lesson09/dadjokes/pages/Jokes/index.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <Joke />
+    </div>
+</template>
+
+<script>
+// import axios from "axios";
+import Joke from "../../components/Joke";
+
+export default {
+    components: {
+        Joke
+    },
+//     data() {
+//         return {
+//             jokes: []
+//         };
+//     },
+    // mounted() {
+    //     axios
+    //         .get('https://icanhazdadjoke.com/search')
+    //         .then(response => (this.jokes = response.data.results))
+    //         .catch(error => {
+    //             console.log(error)
+    //         })
+    //         .finally(() => this.loading = false)
+    // },
+    // async created() {
+    //     const config = {
+    //         headers: {
+    //             Accept: "application/json"
+    //         }
+    //     };
+
+    //     try {
+    //         const response = await axios.get('https://icanhazdadjoke.com/search', config);
+    //         this.jokes = response.data.results;
+    //     } catch(error) {
+    //         console.log(error);
+    //     }
+    // },
+    head() {
+        return {
+            title: "Jokes",
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "Best place for corny dad jokes"
+                }
+            ]
+        }
+    }
+}
+</script>
